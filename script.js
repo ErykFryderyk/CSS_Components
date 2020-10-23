249bacfbf7a457964e11f52fa08f846f4bf71854
Eryk Fryderyk <grzeniu098@gmail.com>
2020-10-23 23:37:26 +0200

    add newsection loading-buttons

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ accordion.forEach(element => {
         element.classList.toggle('accordion-active');
     })
 });
+
+const btns= document.querySelectorAll('.loading-btn');
+
+btns.forEach(item =>{
+    item.addEventListener('click', evt => {
+        evt.target.classList.add('active-loading');
+    })
+})

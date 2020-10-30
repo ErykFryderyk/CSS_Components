33d97bed560ace53adec6d5b84382d030bf954fd
Eryk Fryderyk <grzeniu098@gmail.com>
2020-10-30 22:52:13 +0100

    add style code  and js script

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ btns.forEach(item =>{
         evt.target.classList.add('active-loading');
     })
 })
+
+
+//Counter Animation 
+
+const counts = document.querySelectorAll('.count');
+
+counts.forEach(count => {
+    const updateCount = () => {
+        let currentCount = parseInt(count.innerText);
+        let targetCount = count.getAttribute('data-target');
+        let increaseSpeed = 40;
+        let increaseCount = parseInt (targetCount / increaseSpeed);
+
+            if( currentCount < targetCount) {
+                count.innerText = currentCount + increaseCount; 
+
+            }else{
+                count.innerText = targetCount;
+            }
+            setTimeout(updateCount, increaseSpeed);
+    }
+    updateCount();
+} )

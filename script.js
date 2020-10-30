7a8f5d4b9d3e22dd5de5a3a17aedfce7499f8c6e
Eryk Fryderyk <grzeniu098@gmail.com>
2020-10-31 00:04:07 +0100

    add function hide or show password

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ counts.forEach(count => {
     }
     updateCount();
 } )
+
+
+// Forms Elements
+const myInput = document.getElementById('myInput');
+const myCheck = document.getElementById('myCheckBox');
+
+
+ 
+const visibilityPassword = () => {
+    if(myInput.type === "password"){
+        myInput.type = 'test'
+    }else{
+        myInput.type = 'password';
+    }
+}  
+
+myCheck.addEventListener('click', () => (myCheck.checked) ? visibilityPassword() : visibilityPassword());

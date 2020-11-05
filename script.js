f52cf8fba8fc9ff842ecac1eb8ff557d4dff1018
Eryk Fryderyk <grzeniu098@gmail.com>
2020-11-05 23:12:25 +0100

    add new element, this is scroll indicator

--- script.js
+++ script.js
@@ -54,3 +54,20 @@ const visibilityPassword = () => {
 }  
 
 myCheck.addEventListener('click', () => (myCheck.checked) ? visibilityPassword() : visibilityPassword());
+
+
+
+const content = document.querySelector('.content');
+
+content.addEventListener('scroll', myFunction);
+
+function myFunction() {
+    const contentScroll = content.scrollTop || content.scrollTop;
+    // console.log(content.scrollHeight);
+    const height = content.scrollHeight - content.clientHeight;
+    const scrolled = (contentScroll / height) * 100;
+    console.log(scrolled);
+    // console.log(contentScroll);
+    // console.log(height);
+    document.querySelector('.progress-bar').style.width = scrolled + '%';
+}

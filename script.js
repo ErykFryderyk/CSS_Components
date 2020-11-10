4c46af9d4effc085029ccf5ae620c351904c3378
Eryk Fryderyk <grzeniu098@gmail.com>
2020-11-10 22:23:56 +0100

    add function generation random password

--- script.js
+++ script.js
@@ -97,6 +97,29 @@ const parallax = () => {
 window.addEventListener('scroll', parallax)
 
 
+//Generate Password 
+
+const inputPassword = document.querySelector('.password-box input');
+const generatePasswordBtn = document.querySelector('.password-box-btn');
+
+const generatePassword = (length = 16) => {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprsqtuvwxyz1234567890!@#$%^&*()_';
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        let at = Math.floor(Math.random() * (charset.length + 1));
+        password += charset.charAt(at);
+    }
+
+    return password 
+} 
+
+generatePasswordBtn.addEventListener('click', () => {
+    inputPassword.value = generatePassword(16);
+})
+
+
+
 
 // MOVE MOUSE BACKGROUND EFFECT
 

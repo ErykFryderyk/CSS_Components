c2886767a7f201965f50ffe6a43d2b5aa205e3aa
Eryk Fryderyk <grzeniu098@gmail.com>
2021-04-01 17:02:01 +0200

    add gulp script

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+    return gulp.src('./style/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./style'));
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch('./style/scss/*.scss', gulp.series('sass'));
+});

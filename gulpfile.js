58a6d847f6042cd5458b0e7e75e2e7eabe48088d
Eryk Fryderyk <grzeniu098@gmail.com>
2021-04-08 20:11:25 +0200

    add all folders for watching by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', () => {
-    return gulp.src('./style/scss/*.scss')
+    return gulp.src('./style/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./style'));
 });
 
 gulp.task('sass:watch', () => {
-    gulp.watch('./style/scss/*.scss', gulp.series('sass'));
+    gulp.watch('./style/scss/**/*.scss', gulp.series('sass'));
 });
